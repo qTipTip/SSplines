b8620dca9c3c540e16120073d445a3ed69ec351d
Ivar Stangeby <istangeby@gmail.com>
2018-02-16 21:03:18 +0100

    Added test for quadratic coefficients, single and multiple.

--- tests/test_coefficients.py
+++ tests/test_coefficients.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from SSplines.helper_functions import determine_sub_triangle, coefficients_linear, barycentric_coordinates
+from SSplines.helper_functions import determine_sub_triangle, coefficients_linear, barycentric_coordinates, \
+    coefficients_quadratic
 
 
 def test_coefficients_linear_multiple():
@@ -58,4 +59,61 @@ def test_coefficients_linear_single():
 
     for c, e in zip(k, expected):
         computed = coefficients_linear(c)
+        np.testing.assert_almost_equal(computed, e)
+
+
+def test_coefficients_quadratic_multiple():
+    vertices = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1]
+    ])
+
+    points = np.array([
+        [0, 0.25],
+        [0, 0.5],
+        [0.25, 0.25],
+        [0.5, 0],
+    ])
+
+    b = barycentric_coordinates(vertices, points)
+    k = determine_sub_triangle(vertices, b)
+
+    expected = np.array([
+        [0, 1, 2, 9, 10, 11],
+        [6, 7, 8, 9, 10, 11],
+        [1, 2, 3, 6, 10, 11],
+        [1, 2, 3, 4, 5, 6]
+    ])
+
+    computed = coefficients_quadratic(k)
+    np.testing.assert_almost_equal(computed, expected)
+
+
+def test_coefficients_linear_single():
+    vertices = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1]
+    ])
+
+    points = np.array([
+        [0, 0.25],
+        [0, 0.5],
+        [0.25, 0.25],
+        [0.5, 0],
+    ])
+
+    b = barycentric_coordinates(vertices, points)
+    k = determine_sub_triangle(vertices, b)
+
+    expected = np.array([
+        [0, 1, 2, 9, 10, 11],
+        [6, 7, 8, 9, 10, 11],
+        [1, 2, 3, 6, 10, 11],
+        [1, 2, 3, 4, 5, 6]
+    ])
+
+    for c, e in zip(k, expected):
+        computed = coefficients_quadratic(c)
         np.testing.assert_almost_equal(computed, e)
