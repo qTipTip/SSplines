18c3e6ebad319261198de16cb8b77ec07b35efc7
Ivar Stangeby <istangeby@gmail.com>
2018-02-16 19:10:32 +0100

    Added method for computing the directional coordinates of a point.

--- SSplines/helper_functions.py
+++ SSplines/helper_functions.py
@@ -18,3 +18,19 @@ def barycentric_coordinates(triangle, points, tol=1.0E-15):
 
     x[abs(x) < tol] = 0 # remove round off errors around zero
     return x
+
+
+def directional_coordinates(triangle, direction):
+    """
+    Computes the directional coordinates of one or more direction vector(s) with respect to the given triangle.
+    :param triangle: vertices of the triangle
+    :param direction: a set of points
+    :return: a set of directional coordinates, ndarray of ndim = 2.
+    """
+
+    u = np.atleast_2d(direction)  # make sure the directions are shaped properly
+    A = np.concatenate((triangle, np.ones((3, 1))), axis=1).T  # append a column of ones
+    b = np.concatenate((u, np.ones((len(u), 1))), axis=1)  # append a column of zeros
+
+    a = np.linalg.solve(A[None, :, :], b)  # broadcast A to solve all systems at once
+    return a
