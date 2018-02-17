7cf2a2f1c74ba4b8ad64380d4a3e30cc97d096ed
Ivar Stangeby <istangeby@gmail.com>
2018-02-17 17:18:47 +0100

    Added method for computing derivatives in SplineFunction.

--- SSplines/spline_function.py
+++ SSplines/spline_function.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from SSplines.helper_functions import coefficients_linear, coefficients_quadratic, barycentric_coordinates, \
-    determine_sub_triangle, evaluate_non_zero_basis_splines
+    determine_sub_triangle, evaluate_non_zero_basis_splines, evaluate_non_zero_basis_derivatives, \
+    directional_coordinates
 
 
 class SplineFunction(object):
@@ -31,12 +32,29 @@ class SplineFunction(object):
         """
         Evaluates the spline function at point(s) x.
         :param x: set of points
-        :return:
+        :return: f(x)
         """
 
         b = barycentric_coordinates(self.triangle, x)
         k = determine_sub_triangle(b)
         z = evaluate_non_zero_basis_splines(b=b, d=self.degree, k=k)
+        c = self._non_zero_coefficients(k)
+
+        return np.einsum('...i,...i->...', z, c)  # broadcast the dot product to compute all values at once.
+
+    def D(self, x, u, r):
+        """
+        Evaluates the r'th directional derivative of the function at the point(s) x in direction u.
+        :param x: set of points
+        :param u: direction
+        :param r: order of derivative
+        :return: f^(r)(x)
+        """
+
+        b = barycentric_coordinates(self.triangle, x)
+        k = determine_sub_triangle(b)
+        a = directional_coordinates(self.triangle, u)
+        z = evaluate_non_zero_basis_derivatives(a=a, b=b, k=k, r=r, d=self.degree)
         c = self._non_zero_coefficients(k)
 
         return np.einsum('...i,...i->...', z, c)  # broadcast the dot product to compute all values at once.
