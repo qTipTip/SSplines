14154048fa7db8334be4ceffff7a8c350f226053
Ivar Stangeby <istangeby@gmail.com>
2018-02-17 16:12:47 +0100

    Added a similar partition of unity test for linear splines.

--- tests/test_non_zero_basis_evaluation.py
+++ tests/test_non_zero_basis_evaluation.py
@@ -3,9 +3,9 @@ import numpy as np
 from SSplines.helper_functions import sample_triangle, barycentric_coordinates, evaluate_non_zero_basis_splines
 
 
-def test_non_zero_splines_evaluation_multiple():
+def test_non_zero_splines_evaluation_multiple_quadratic():
     """
-    Computes all the non-zero basis splines at a set of points, and asserts
+    Computes all the non-zero quadratic basis splines at a set of points, and asserts
     that for each point, the basis functions sum to one.
     """
 
@@ -16,6 +16,31 @@ def test_non_zero_splines_evaluation_multiple():
     ])
 
     d = 2
+
+    points = sample_triangle(triangle, 30)
+    bary_coords = barycentric_coordinates(triangle, points)
+
+    s = evaluate_non_zero_basis_splines(triangle, d, bary_coords)
+
+    expected_sum = np.ones((len(points)))
+    computed_sum = s.sum(axis=1)
+
+    np.testing.assert_almost_equal(expected_sum, computed_sum)
+
+
+def test_non_zero_splines_evaluation_multiple_linear():
+    """
+    Computes all the non-zero linear basis splines at a set of points, and asserts
+    that for each point, the basis functions sum to one.
+    """
+
+    triangle = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1]
+    ])
+
+    d = 1
 
     points = sample_triangle(triangle, 30)
     bary_coords = barycentric_coordinates(triangle, points)
