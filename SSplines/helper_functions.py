Forgot to multiply by (i+1) in derivative. Fixed now.

SSplines/helper_functions.py
@@ -245,7 +245,7 @@ def evaluate_non_zero_basis_derivatives(d, r, b, a, k):
 
     for j, i in enumerate(range(d - r, d)):
         u_sub = sub_matrix(U[i], i + 1, k)
-        s = np.einsum('...ij,...jk->...ik', np.atleast_3d(s), u_sub)
+        s = (i + 1) * np.einsum('...ij,...jk->...ik', np.atleast_3d(s), u_sub)
     return np.squeeze(s)  # squeeze to remove redundant dimension
 
 