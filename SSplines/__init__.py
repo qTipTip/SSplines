64a1d67e4b876bded84c37ab8c324afe9eec3084
Ivar Stangeby <istangeby@gmail.com>
2018-02-21 12:03:49 +0100

    Added more to __init__.py

--- SSplines/__init__.py
+++ SSplines/__init__.py
@@ -1,3 +1,3 @@
-from .helper_functions import sample_triangle, signed_area
+from .helper_functions import sample_triangle, signed_area, barycentric_coordinates, points_from_barycentric_coordinates
 from .spline_function import SplineFunction
 from .spline_space import SplineSpace
