f3d0fbaa3e51f2209602c99552438bacb00dbd69
Ivar Stangeby <istangeby@gmail.com>
2018-02-21 14:30:33 +0100

    Added area and quadrature to __init__

--- SSplines/__init__.py
+++ SSplines/__init__.py
@@ -1,3 +1,4 @@
-from .helper_functions import sample_triangle, signed_area, barycentric_coordinates, points_from_barycentric_coordinates
+from .helper_functions import sample_triangle, signed_area, barycentric_coordinates, \
+    points_from_barycentric_coordinates, area, gaussian_quadrature, gaussian_quadrature_data
 from .spline_function import SplineFunction
 from .spline_space import SplineSpace
