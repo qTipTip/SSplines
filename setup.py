a2be05c2c7fb2fad34cd906ad906ee0991afda23
Ivar Stangeby <istangeby@gmail.com>
2018-02-21 11:35:13 +0100

    Added setup_script for installing locally.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='SSplines',
+    version='1.1.1',
+    packages=['SSplines'],
+    url='https://github.com/qTipTip/SSplines2',
+    license='MIT',
+    author='Ivar Stangeby',
+    author_email='istangeby@gmail.com',
+    description='A small Python library for the evaluation of S-splines on the Powell-Sabin 12-split of a triangle.',
+    long_description='''This Python library lets you instantiate constant,
+    linear and quadratic spline spaces on the Powell-Sabin 12-split of a
+    triangle. Given a set of coefficients a SplineSpace returns a callable
+    SplineFunction which can be evaluated and differentiated.''',
+    install_requires=['numpy', 'matplotlib'],
+    python_requires='>=3',
+)
