Fixed symbolic pieces! Tested against numerical. Everything works.

SSplines/symbolic.py
@@ -1,113 +1,177 @@
-from math import factorial
+import math
 
+import numpy as np
 import sympy as sp
-from SSplines import ps12_vertices
-from SSplines.constants import DELETED_KNOT_TO_TRIANGLE, KNOT_CONFIGURATION_TO_FACE_INDICES
+
+import SSplines
+from SSplines.dicts import KNOT_CONFIGURATION_TO_SUBTRIANGLES, DELETED_KNOT_TO_TRIANGLE, \
+    KNOT_CONFIGURATION_TO_FACE_INDICES
+
+X, Y = sp.symbols('X Y')
 
 
-def degenerate_support(s):
+def degenerate(knot_configuration):
     """
-    Checks whether the given list of vertex indices describe
-    a degenerate triangle in the PS12-split.
-    :param s: list of vertex indices.
-    :return: true/false
+    Given a knot configuration, return True if the corresponding triangle
+    is degenerate, of area zero, or False otherwise.
     """
 
-    # not enough vertices to describe a triangle.
-    if len(s) < 3:
-        return True
-    # the following configurations only correspond to splines of degree at most two.
-    if s == (0, 1, 3) or s == (1, 2, 4) or s == (0, 2, 5):
-        return True
-
-    # the following configurations corresponds to more general splines as the vertices are not used for higher degree
-    # splines,
-    # and these might not be needed.
-    # implemented for completeness.
-    if s == (3, 5, 6) or s == (3, 4, 7) or s == (4, 5, 8):
-        return True
-    if s == (0, 6, 9) or s == (0, 4, 6) or s == (0, 4, 9) or s == (4, 6, 9) \
-            or s == (3, 8, 9) or s == (2, 3, 9) or s == (2, 3, 8) or s == (2, 8, 9) \
-            or s == (1, 7, 9) or s == (1, 5, 7) or s == (1, 5, 9) or s == (5, 7, 9):
+    if knot_configuration == (0, 1, 3) or knot_configuration == (1, 2, 4) or knot_configuration == (0, 2, 5):
         return True
     return False
 
 
-def barycentric_coordinates_symbolic(p0, p1, p2):
-    X, Y = sp.symbols('X Y')
+def non_zero_multiplicities(knot_multiplicities):
+    """
+    Returns a list of the non-zero multiplicities in a list of knot
+    multiplicities, as well as the corresponding knot configuration.
+    """
 
+    non_zero_m = []
+    knot_confg = []
+    for i, m in enumerate(knot_multiplicities):
+        if m != 0:
+            non_zero_m.append(m)
+            knot_confg.append(i)
+    return non_zero_m, knot_confg
+
+
+def knot_configuration_to_non_degenerate_interior_triangle(knot_configuration):
+    """
+    Given a knot configuration, returns the indices of a triangle contained in
+    the convex hull of the knots.
+    """
+    return DELETED_KNOT_TO_TRIANGLE[tuple(knot_configuration)]
+
+
+def knot_configuration_to_face(knot_configuration):
+    """
+    Given a knot configuration, returns a list of face indices corresponding to
+    the faces that lie within the convex hull of the knot configuration.
+    """
+    return KNOT_CONFIGURATION_TO_FACE_INDICES[tuple(knot_configuration)]
+
+
+def barycentric_coordinates(triangle):
+    """
+    Returns barycentric coordinates with respect to the given triangle as
+    functions of X, Y.
+    """
+    p0, p1, p2 = triangle
     A = sp.Matrix([[p0[0], p1[0], p2[0]],
                    [p0[1], p1[1], p2[1]],
                    [1, 1, 1]])
     b = sp.Matrix([[X], [Y], [1]])
-
     barycentric_coords = A.LUsolve(b)
 
     return list(barycentric_coords)
 
 
-def polynomial_pieces(triangle, multiplicities):
+def multinomial(i, j, k):
     """
-    Given a set of vertices, and a list of knot multiplicities, returns the twelve polynomial pieces corresponding to
-    the S-spline basis function.
-    :param triangle: vertices of triangle
-    :param multiplicities: list of knot multiplicites
-    :return: list of twelve polynomial pieces
+    Computes the trinomial for use in evaluation of Bernstein polynomials.
+    """
+    return math.factorial(i + j + k) / (math.factorial(i) * math.factorial(j) * math.factorial(k))
+
+
+def bernstein_polynomial(full_triangle, sub_triangle, non_zero_multiplicities):
+    """
+    Computes the bernstein polynomial over given triangle with knot
+    multiplicities. Non_zero_multiplicities has to be of length 3!
+    """
+    b1, b2, b3 = barycentric_coordinates(sub_triangle)
+    m1, m2, m3 = [m - 1 for m in non_zero_multiplicities]
+
+    area_full = SSplines.area(full_triangle)
+    area_sub = SSplines.area(sub_triangle)
+
+    Q = b1 ** m1 * b2 ** m2 * b3 ** m3 * multinomial(m1, m2, m3) * area_full / area_sub
+    return Q
+
+
+def remove_knot(knot_multiplicities, knot):
+    """
+    Given a list of knot multiplicities, and a knot, reduce the corresponding
+    knot multiplicity by one.
     """
 
-    ps12 = ps12_vertices(triangle)
+    reduced_knot_multiplicities = [m - (knot == j) for j, m in enumerate(knot_multiplicities)]
 
-    non_zero_multiplicities = [(k, m) for k, m in enumerate(multiplicities) if m != 0]
-    knot_configuration = tuple([k for k, _ in non_zero_multiplicities])
+    return reduced_knot_multiplicities
 
+
+def knot_configuration_area(knot_configuration, ps12):
+    """
+    Given a knot configuration corresponding to a quadratic S-spline basis,
+    return the area of the convex hull.
+    """
+
+    sub_triangle_configurations = KNOT_CONFIGURATION_TO_SUBTRIANGLES[tuple(knot_configuration)]
+    a = 0
+    for triangle in sub_triangle_configurations:
+        points = ps12[triangle]
+        a += SSplines.area(points)
+    return a
+
+
+def polynomial_pieces(triangle, knot_multiplicities, first_call=True):
+    """
+    Computes the twelve polynomial pieces of the spline with given knot
+    multiplicities over the given triangle.
+    """
+
+    ps12 = SSplines.ps12_vertices(triangle)
+    non_zero_knots, knot_configuration = non_zero_multiplicities(knot_multiplicities)
     polynomials = [0] * 12
-
-    if degenerate_support(knot_configuration):
+    # if the triangle has zero area, return zeros.
+    if degenerate(tuple(knot_configuration)):
         return polynomials
 
+    # or, if there are three distinct knots, compute bernstein)
+    # polynomial contributions
     elif len(knot_configuration) == 3:
+        sub_triangle = ps12[knot_configuration]
+        full_triangle = ps12[[0, 1, 2]]
+        Q = bernstein_polynomial(full_triangle, sub_triangle, non_zero_knots)
+        for face in knot_configuration_to_face(knot_configuration):
+            polynomials[face] += Q
 
-        v1, v2, v3 = [ps12[i] for i in knot_configuration]
-        b1, b2, b3 = barycentric_coordinates_symbolic(v1, v2, v3)
-
-        mu1, mu2, mu3 = [m - 1 for _, m in non_zero_multiplicities]
-
-        multinomial = factorial(mu1 + mu2 + mu3) / (factorial(mu1) * factorial(mu2) * factorial(mu3))
-        Q = (b1 ** mu1 * b2 ** mu2 * b3 ** mu3) * multinomial
-
-        faces = KNOT_CONFIGURATION_TO_FACE_INDICES[knot_configuration]
-        for face in faces:
-            polynomials[face] = Q
-
-        return polynomials
-
-    elif len(knot_configuration) > 3:
-        # find a non-degenerate triangle in the knot_configuration and compute
-        # barycentric coordinates with respect to this new triangle.
-        reduced_knot_configuration = DELETED_KNOT_TO_TRIANGLE[knot_configuration]
-        v1, v2, v3 = [ps12[i] for i in reduced_knot_configuration]
-        bary_coordinates = barycentric_coordinates_symbolic(v1, v2, v3)
+    # otherwise, find a non-degenerate triangle in the interior of the knot
+    # configuration, and compute contributions recursively by removing knots
+    # one by one.
+    else:
+        interior_triangle_knots = knot_configuration_to_non_degenerate_interior_triangle(knot_configuration)
+        interior_triangle_verts = ps12[list(interior_triangle_knots)]
+        barycentric_coords = barycentric_coordinates(interior_triangle_verts)
 
         Q = []
-
-        # compute contributions recursively
-        for i in reduced_knot_configuration:
-            reduced_multiplicities = [m - (i == j) for j, m in enumerate(multiplicities)]
-            reduced_polynomial_pieces = polynomial_pieces(triangle, reduced_multiplicities)
+        for knot in interior_triangle_knots:
+            reduced_knot_multiplicities = remove_knot(knot_multiplicities, knot)
+            reduced_polynomial_pieces = polynomial_pieces(triangle, reduced_knot_multiplicities, first_call=False)
             Q.append(reduced_polynomial_pieces)
 
-        # add up total
         for i in range(len(polynomials)):
-            # for each of the 12 faces
             for j in range(len(Q)):
-                # add the corresponding three contributions
-                polynomials[i] += Q[j][i] * bary_coordinates[j]
+                polynomials[i] += Q[j][i] * barycentric_coords[j]
 
-        # Normalize by area
-        # area_triangle = abs(signed_area(triangle))
-        # area_knot_configuration = abs(signed_area(np.array([v1, v2, v3])))
-        # factor = area_triangle / area_knot_configuration
-        # for i in range(len(polynomials)):
-        #    polynomials[i] *= factor
+    # If this is the final return statement in the recurrence, then normalize
+    # by area to obtain the S-spline basis.
+    if first_call:
+        area_knot_config = knot_configuration_area(knot_configuration, ps12)
+        area_full_triang = SSplines.area(ps12[[0, 1, 2]])
 
-        return polynomials
+        for i in range(len(polynomials)):
+            polynomials[i] *= (area_knot_config / area_full_triang)
+    return polynomials
+
+
+if __name__ == "__main__":
+    triangle = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1]
+    ])
+
+    p = SSplines.sample_triangle(triangle, 30)
+    b = SSplines.barycentric_coordinates(triangle, p)
+    k = SSplines.determine_sub_triangle(b)

tests/test_derivatives_vs_polynomial_pieces.py
@@ -1,12 +1,13 @@
 import numpy as np
 import sympy as sy
+
 from SSplines import SplineSpace, sample_triangle
 from SSplines.constants import KNOT_MULTIPLICITIES_QUADRATIC
 from SSplines.helper_functions import determine_sub_triangle, barycentric_coordinates
 from SSplines.symbolic import polynomial_pieces
 
 
-def test_gradient_quadratic_corner_basis_functions():
+def test_laplacian_quadratic_basis_functions():
     X, Y = sy.symbols('X Y')
 
     triangle = np.array([
@@ -17,7 +18,7 @@ def test_gradient_quadratic_corner_basis_functions():
 
     S = SplineSpace(triangle, 2)
     points = sample_triangle(triangle, 15)
-    for basis_num in [0, 4, 8]:
+    for basis_num in range(12):
         b_num = S.basis()[basis_num]
         b_sym = polynomial_pieces(triangle, KNOT_MULTIPLICITIES_QUADRATIC[basis_num])
 
@@ -27,3 +28,49 @@ def test_gradient_quadratic_corner_basis_functions():
             b_lapl = sy.diff(b_sym[k], X, X) + sy.diff(b_sym[k], Y, Y)
             num_b_sym = sy.lambdify([X, Y], b_lapl)
             np.testing.assert_almost_equal(b_num.lapl(p), num_b_sym(p[0], p[1]))
+
+
+def test_gradient_quadratic_basis_functions():
+    X, Y = sy.symbols('X Y')
+
+    triangle = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1]
+    ])
+
+    S = SplineSpace(triangle, 2)
+    points = sample_triangle(triangle, 15)
+    for basis_num in range(12):
+        b_num = S.basis()[basis_num]
+        b_sym = polynomial_pieces(triangle, KNOT_MULTIPLICITIES_QUADRATIC[basis_num])
+
+        for p in points:
+            b = barycentric_coordinates(triangle, p)
+            k = determine_sub_triangle(b)[0]
+            b_lapl = [sy.diff(b_sym[k], X), sy.diff(b_sym[k], Y)]
+            num_b_sym = sy.lambdify([X, Y], b_lapl)
+            np.testing.assert_almost_equal(np.squeeze(b_num.grad(p)), num_b_sym(p[0], p[1]))
+
+
+def test_divergence_quadratic_basis_functions():
+    X, Y = sy.symbols('X Y')
+
+    triangle = np.array([
+        [0, 0],
+        [1, 0],
+        [0, 1]
+    ])
+
+    S = SplineSpace(triangle, 2)
+    points = sample_triangle(triangle, 15)
+    for basis_num in range(12):
+        b_num = S.basis()[basis_num]
+        b_sym = polynomial_pieces(triangle, KNOT_MULTIPLICITIES_QUADRATIC[basis_num])
+
+        for p in points:
+            b = barycentric_coordinates(triangle, p)
+            k = determine_sub_triangle(b)[0]
+            b_lapl = sy.diff(b_sym[k], X) + sy.diff(b_sym[k], Y)
+            num_b_sym = sy.lambdify([X, Y], b_lapl)
+            np.testing.assert_almost_equal(np.squeeze(b_num.div(p)), num_b_sym(p[0], p[1]))

tests/test_values_vs_polynomial_pieces.py
@@ -1,12 +1,12 @@
 import numpy as np
-import pytest
 import sympy as sy
+
 from SSplines import SplineSpace, ps12_sub_triangles, sample_triangle
 from SSplines.constants import KNOT_MULTIPLICITIES_QUADRATIC
 from SSplines.symbolic import polynomial_pieces
 
 
-def test_values_quadratic_corner_bases():
+def test_values_quadratic_basis():
     """
     tests that the polynomial pieces and the numerical splines evaluate to the same value over all the
     subtriangles.
@@ -24,34 +24,7 @@ def test_values_quadratic_corner_bases():
     for basis_num in range(12):
         b_num = S.basis()[basis_num]
         b_sym = polynomial_pieces(triangle, KNOT_MULTIPLICITIES_QUADRATIC[basis_num])
-        if basis_num in [1, 2, 3, 5, 6, 7, 9, 10, 11]:
-            continue
-        subtriangles = ps12_sub_triangles(triangle)
 
-        for k in range(12):
-            t = subtriangles[k]
-            points = sample_triangle(t, 1)
-            num_b_sym = sy.lambdify([X, Y], b_sym[k])
-
-            for p in points:
-                np.testing.assert_almost_equal(b_num(p), num_b_sym(p[0], p[1]))
-
-
-@pytest.mark.skip(reason='Not working properly')
-def test_values_quadratic_edge_midpoints():
-    X, Y = sy.symbols('X Y')
-
-    triangle = np.array([
-        [0, 0],
-        [1, 0],
-        [0, 1]
-    ])
-
-    S = SplineSpace(triangle, 2)
-
-    for basis_num in [2, 5, 10]:
-        b_num = S.basis()[basis_num]
-        b_sym = polynomial_pieces(triangle, KNOT_MULTIPLICITIES_QUADRATIC[basis_num])
         subtriangles = ps12_sub_triangles(triangle)
 
         for k in range(12):