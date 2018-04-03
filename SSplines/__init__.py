43be6741cf42be61c3304b5aaed2c0a401a4de60
Ivar Stangeby <istangeby@gmail.com>
2018-04-03 16:06:25 +0200

    Changed import again.

--- SSplines/__init__.py
+++ SSplines/__init__.py
@@ -1,7 +1,8 @@
-from helper_functions import sample_triangle, signed_area, barycentric_coordinates, \
+from .helper_functions import sample_triangle, signed_area, barycentric_coordinates, \
     points_from_barycentric_coordinates, area, gaussian_quadrature, gaussian_quadrature_data, ps12_vertices, \
     ps12_sub_triangles, gaussian_quadrature_ps12, edge_quadrature, edge_quadrature_ps12, edge_quadrature_data, \
     determine_sub_triangle, domain_points
-from spline_function import SplineFunction
-from spline_space import SplineSpace
-from symbolic import polynomial_basis_quadratic
+
+from .spline_function import SplineFunction
+from .spline_space import SplineSpace
+from .symbolic import polynomial_basis_quadratic
