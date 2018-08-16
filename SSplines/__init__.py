905678251970e8b3d109f8a66f459c6cfab24427
Ivar Stangeby <istangeby@gmail.com>
2018-08-16 15:50:42 +0200

    Added SimplexSpline to module.

--- SSplines/__init__.py
+++ SSplines/__init__.py
@@ -2,7 +2,7 @@ from .helper_functions import sample_triangle, signed_area, barycentric_coordina
     points_from_barycentric_coordinates, area, gaussian_quadrature, gaussian_quadrature_data, ps12_vertices, \
     ps12_sub_triangles, gaussian_quadrature_ps12, edge_quadrature, edge_quadrature_ps12, edge_quadrature_data, \
     determine_sub_triangle, domain_points
-
+from .simplex_spline import SimplexSpline
 from .spline_function import SplineFunction
 from .spline_space import SplineSpace
 from .symbolic import polynomial_basis_quadratic
